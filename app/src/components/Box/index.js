dd8d58c8260875f033fc3de19bd2ab1d506284e2
João Alberto <joaoalberto.contato@gmail.com>
2019-08-19 02:24:14 -0300

    Updates
    - Box component
    - Fixing typos in Text and Title component

---
+++ app/src/components/Box/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Styles
+import "./index.css";
+
+// Component
+const Box = ({ children }) => {
+  return <h1 className="box">{children}</h1>;
+};
+
+export default Box;
--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Components
+import Box from './components/Box'
 import Button from './components/Button'
 import Input from './components/Input'
 import Title from './components/Title'
@@ -17,6 +18,7 @@ const App = () => (
     <Input label="Nome" placeholder="Escreva seu nome completo" name="name" error="O campo email é obrigatório" />
     <Title>Criar meu cadastro</Title>
     <Text>Para acompanhar sua contratação de crédito você utilizará seu e-mail e CPF.</Text>
+    <Box>Box component</Box>
   </div>
 )
 
