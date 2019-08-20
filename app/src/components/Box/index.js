d1ca0810f0ea3184abc4ddbff84f82b43310a91a
João Alberto <joaoalberto.contato@gmail.com>
2019-08-20 04:39:42 -0300

    ADding box error style

--- app/src/components/Box/index.js
+++ app/src/components/Box/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 
+// Utils
+import classNames from "../../utils/classnames";
+
 // Styles
 import "./index.css";
 
 // Component
-const Box = ({ children }) => {
-  return <div className="box">{children}</div>;
+const Box = ({ children, error }) => {
+  const className = classNames(["box", error ? "box--error" : null]);
+
+  return <div className={className}>{children}</div>;
 };
 
 export default Box;
