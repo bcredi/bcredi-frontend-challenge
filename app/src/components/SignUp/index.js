6d83a1f30cc3fdf02dde9a70ce94fd67bb302cd7
João Alberto <joaoalberto.contato@gmail.com>
2019-08-19 23:47:49 -0300

    Updates
    - SignUp wrapper styles
    - Header component
    - Content component
    - Footer component

--- app/src/components/SignUp/index.js
+++ app/src/components/SignUp/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+// Components
+import Icon from "../Icon";
+import Link from "../Link";
+import Text from "../Text";
+
 // Styles
 import "./index.css";
 
 // Component
 const SignUp = ({ children }) => {
-  return <div className="signup">{children}</div>;
+  return (
+    <div className="signup">
+      <div className="signup__header">
+        <Icon className="signup__header__logo" icon="logo" />
+      </div>
+      <div className="signup__content"></div>
+      <div className="signup__footer">
+        <Text center>
+          Já fiz meu cadastro. <Link>Entrar agora</Link>
+        </Text>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
