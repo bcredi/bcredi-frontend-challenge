e51c85c6a2206f8e82c69fe7e877b36b95cbfb3b
João Alberto <joaoalberto.contato@gmail.com>
2019-08-20 01:42:22 -0300

    Creating row component

---
+++ app/src/components/Row/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Styles
+import "./index.css";
+
+// Component
+const Row = ({ children }) => {
+  return <div className="row">{children}</div>;
+};
+
+export default Row;
