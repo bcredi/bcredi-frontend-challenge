2c20d3f169ce8823575ac034154c130871e9f0df
João Alberto <joaoalberto.contato@gmail.com>
2019-08-19 23:42:03 -0300

    Update logo

--- app/src/components/Icon/index.js
+++ app/src/components/Icon/index.js
@@ -3,15 +3,19 @@ import React from "react";
 // Icons
 import active from "../../assets/icons/active.svg";
 import locked from "../../assets/icons/signup.svg";
+import logo from "../../assets/icons/logo.svg";
 import view from "../../assets/icons/view-password.svg";
 
 const icons = {
   active,
   locked,
+  logo,
   view
 };
 
 // Component
-const Icon = ({ className="", icon }) => <img className={className} src={icons[icon]} />;
+const Icon = ({ className = "", icon }) => (
+  <img className={className} src={icons[icon]} />
+);
 
 export default Icon;
